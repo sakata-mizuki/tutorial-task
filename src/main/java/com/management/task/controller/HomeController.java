b50afbf8562bae423bc9bad0c3a68f7623c0391e
sakata-mizuki <sakata_mizuki@y-i-group.co.jp>
2024-07-16 11:29:42 +0900

    fix as no db used

--- src/main/java/com/management/task/controller/HomeController.java
+++ src/main/java/com/management/task/controller/HomeController.java
@@ -1,57 +1,44 @@
 package com.management.task.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.management.task.model.Task;
 import com.management.task.model.TaskForm;
-import com.management.task.service.TaskService;
 
 @Controller
 public class HomeController {
 
-    private final TaskService taskService;
+    record TaskItem(UUID id, String title, LocalDateTime deadline, Boolean isCompleted) {}
 
-    @Autowired
-    public HomeController(TaskService service) {
-        this.taskService = service;
+    private List<TaskItem> taskItems = new ArrayList<>();
+
+    @RequestMapping(value="/hello")
+    String hello(Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
     }
 
     @GetMapping("/home")
     String listItems(Model model) {
-        List<Task> taskList = taskService.findAll();
-        model.addAttribute("taskList", taskList);
+        model.addAttribute("taskList", taskItems);
         model.addAttribute("taskCreateForm", new TaskForm());
         return "home";
     }
 
     @PostMapping("/add")
-    String addItem(
-    		@ModelAttribute("taskCreateForm") TaskForm form
-    ) {
-        taskService.add(form);
-        return "redirect:/home";
-    }
-
-    @GetMapping("/delete")
-    String deleteItem(@RequestParam("id") UUID id) {
-        taskService.delete(id);
-        return "redirect:/home";
-    }
-
-    @PostMapping("/update")
-    String updateItem(
-            @ModelAttribute("taskUpdateForm") TaskForm form
-    ) {
-        taskService.update(form);
+    String addItem(@ModelAttribute("taskCreateForm") TaskForm form) {
+        UUID id = UUID.randomUUID();
+        TaskItem item = new TaskItem(id, form.getTitle(), form.getDeadline(), false);
+        taskItems.add(item);
         return "redirect:/home";
     }
 }
